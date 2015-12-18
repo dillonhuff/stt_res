ea4ab3a9b0645cba01a742b46cb9217f5c9a04a0
Dillon Huff <dillonhuff@gmail.com>
2015-12-17 17:22:40 -0800

    more simplification of tests

--- test/disagreement_set_tests.cpp
+++ test/disagreement_set_tests.cpp
@@ -3,111 +3,89 @@
 #include "test/catch.hpp"
 
 namespace stt_res {
-
-  TEST_CASE("insert_var") {
+  TEST_CASE("disagreement_set", "[disagreement_set]") {
     context c;
     auto t = c.mk_tvar("t");
+    auto ft = c.mk_tfunc(t, t);    
     auto x = c.mk_var("x", t);
-    tp s{x, x};
-    disagreement_set d;
-    d.insert(s);
-    REQUIRE(d.size() == 1);
-  }
-
-  TEST_CASE("erase_var") {
-    context c;
-    auto t = c.mk_tvar("t");
-    auto x = c.mk_var("x", t);
-    tp s{x, x};
-    disagreement_set d;
-    d.insert(s);
-    d.erase_pair(s);
-    REQUIRE(d.size() == 0);
-  }
-
-  TEST_CASE("insert_twice") {
-    context c;
-    auto t = c.mk_tvar("t");
+    auto y = c.mk_var("y", t);    
     auto x1 = c.mk_var("x", t);
     auto y1 = c.mk_var("y", t);
-    tp s1{x1, y1};
-    disagreement_set d;
-    d.insert(s1);
     auto x2 = c.mk_var("x", t);
     auto y2 = c.mk_var("y", t);
-    tp s2{x2, y2};    
-    d.insert(s2);
-    REQUIRE(d.size() == 1);
-  }
-
-  TEST_CASE("insert_twice_same_obj") {
-    context c;
-    auto t = c.mk_tvar("t");
-    auto x1 = c.mk_var("x", t);
-    auto y1 = c.mk_var("y", t);
-    tp s{x1, y1};
-    disagreement_set d;
-    d.insert(s);
-    d.insert(s);
-    REQUIRE(d.size() == 1);
-  }
-
-  TEST_CASE("insert_different_same_fst") {
-    context c;
-    auto t = c.mk_tvar("t");
-    auto x = c.mk_var("x", t);
-    auto y = c.mk_var("y", t);
-    tp s1{x, y};
-    disagreement_set d;
-    d.insert(s1);
-    auto z = c.mk_var("z", t);
-    tp s2{x, z};
-    d.insert(s2);
-    REQUIRE(d.size() == 2);
-  }
-
-  TEST_CASE("insert_different_same_snd") {
-    context c;
-    auto t = c.mk_tvar("t");
-    auto x = c.mk_var("x", t);
-    auto y = c.mk_var("y", t);
-    tp s1{y, x};
-    disagreement_set d;
-    d.insert(s1);
-    auto z = c.mk_var("z", t);
-    tp s2{z, x};
-    d.insert(s2);
-    REQUIRE(d.size() == 2);
-  }
-  
-  TEST_CASE("insert_different") {
-    context c;
-    auto t = c.mk_tvar("t");
-    auto x = c.mk_var("x", t);
-    auto y = c.mk_var("y", t);
-    tp s1{x, y};
-    disagreement_set d;
-    d.insert(s1);
-    auto z = c.mk_var("z", t);
-    auto k = c.mk_var("k", t);
-    tp s2{z, k};
-    d.insert(s2);
-    REQUIRE(d.size() == 2);
-  }
-
-  TEST_CASE("insert_var_apps") {
-    context c;
-    auto t = c.mk_tvar("t");
-    auto ft = c.mk_tfunc(t, t);
-    auto x = c.mk_var("x", t);
-    auto y = c.mk_var("y", t);
     auto f = c.mk_var("f", ft);
-    tp s1{c.mk_ap(f, x), x};
-    tp s2{c.mk_ap(f, y), y};
-    disagreement_set d;
-    sub s{s1, s2};
-    d.add_pairs(s);
-    REQUIRE(d.size() == 2);
+
+    SECTION("insert_var") {
+      tp s{x, x};
+      disagreement_set d;
+      d.insert(s);
+      REQUIRE(d.size() == 1);
+    }
+
+    SECTION("erase_var") {
+      tp s{x, x};
+      disagreement_set d;
+      d.insert(s);
+      d.erase_pair(s);
+      REQUIRE(d.size() == 0);
+    }
+
+    SECTION("insert_twice") {
+      tp s1{x1, y1};
+      disagreement_set d;
+      d.insert(s1);
+      tp s2{x2, y2};    
+      d.insert(s2);
+      REQUIRE(d.size() == 1);
+    }
+
+    SECTION("insert_twice_same_obj") {
+      tp s{x1, y1};
+      disagreement_set d;
+      d.insert(s);
+      d.insert(s);
+      REQUIRE(d.size() == 1);
+    }
+
+    SECTION("insert_different_same_fst") {
+      tp s1{x, y};
+      disagreement_set d;
+      d.insert(s1);
+      auto z = c.mk_var("z", t);
+      tp s2{x, z};
+      d.insert(s2);
+      REQUIRE(d.size() == 2);
+    }
+
+    SECTION("insert_different_same_snd") {
+      tp s1{y, x};
+      disagreement_set d;
+      d.insert(s1);
+      auto z = c.mk_var("z", t);
+      tp s2{z, x};
+      d.insert(s2);
+      REQUIRE(d.size() == 2);
+    }
+  
+    SECTION("insert_different") {
+      tp s1{x, y};
+      disagreement_set d;
+      d.insert(s1);
+      auto z = c.mk_var("z", t);
+      auto k = c.mk_var("k", t);
+      tp s2{z, k};
+      d.insert(s2);
+      REQUIRE(d.size() == 2);
+    }
+
+    SECTION("insert_var_apps") {
+      tp s1{c.mk_ap(f, x), x};
+      tp s2{c.mk_ap(f, y), y};
+      disagreement_set d;
+      sub s{s1, s2};
+      d.add_pairs(s);
+      REQUIRE(d.size() == 2);
+    }
   }
   
 };
--- test/resolution_tests.cpp
+++ test/resolution_tests.cpp
@@ -10,21 +10,7 @@ using namespace std;
 
 namespace stt_res {
 
-  TEST_CASE("resolve_empty") {
-    context c;
-    vector<proof*> clauses;
-    auto p = resolve(c, clauses);
-    REQUIRE(p == nullptr);
-  }
-
-  TEST_CASE("resolve_empty_clause") {
-    context c;
-    vector<proof*> clauses{c.mk_assumption(empty_clause)};
-    auto p = resolve(c, clauses);
-    REQUIRE(p->result == empty_clause);
-  }
-
-  TEST_CASE("resolve_a_not_a") {
+  TEST_CASE("resolution", "[resolution]") {
     context c;
     auto a = c.mk_con("a", c.b());
     auto not_a = c.mk_not(a);
@@ -32,34 +18,44 @@ namespace stt_res {
     auto not_a_p = c.mk_assumption(not_a);
     cout << *a << endl;
     cout << *not_a << endl;
-    vector<proof*> clauses{a_p, not_a_p};
-    auto p = resolve(c, clauses);
-    REQUIRE(p->result == empty_clause);
-  }
+    
+    SECTION("resolve_empty") {
+      vector<proof*> clauses;
+      auto p = resolve(c, clauses);
+      REQUIRE(p == nullptr);
+    }
 
-  TEST_CASE("resolve_a_na_one_clause") {
-    context c;
-    auto a = c.mk_con("a", c.b());
-    auto not_a = c.mk_not(a);
-    auto a_p = c.mk_assumption(a);
-    auto not_a_p = c.mk_assumption(not_a);
-    cout << *a << endl;
-    cout << *not_a << endl;
-    vector<proof*> clauses{a_p, not_a_p};
-    auto p = resolve(c, clauses);
-    REQUIRE(p->result == empty_clause);
-  }
+    SECTION("resolve_empty_clause") {
+      vector<proof*> clauses{c.mk_assumption(empty_clause)};
+      auto p = resolve(c, clauses);
+      REQUIRE(p->result == empty_clause);
+    }
 
-  // TEST_CASE("refute_not_excluded_middle") {
-  //   context c;
-  //   auto a = c.mk_var("a", c.b());
-  //   auto not_a = c.mk_not(a);
-  //   auto exc_middle = c.mk_forall(a, c.mk_and(a, not_a));
-  //   cout << *exc_middle << endl;
-  //   auto exc_middle_p = c.mk_assumption(exc_middle);
-  //   vector<proof*> clauses{exc_middle_p};
-  //   auto p = resolve(c, clauses);
-  //   REQUIRE(p->result == empty_clause);
-  // }
+    SECTION("resolve_a_not_a") {
+      vector<proof*> clauses{a_p, not_a_p};
+      auto p = resolve(c, clauses);
+      REQUIRE(p->result == empty_clause);
+    }
+
+    // TODO: Change to (~ a) \/ a
+    SECTION("resolve_a_na_one_clause") {
+      vector<proof*> clauses{a_p, not_a_p};
+      auto p = resolve(c, clauses);
+      REQUIRE(p->result == empty_clause);
+    }
+
+    // SECTION("refute_not_excluded_middle") {
+    //   context c;
+    //   auto a = c.mk_var("a", c.b());
+    //   auto not_a = c.mk_not(a);
+    //   auto exc_middle = c.mk_forall(a, c.mk_and(a, not_a));
+    //   cout << *exc_middle << endl;
+    //   auto exc_middle_p = c.mk_assumption(exc_middle);
+    //   vector<proof*> clauses{exc_middle_p};
+    //   auto p = resolve(c, clauses);
+    //   REQUIRE(p->result == empty_clause);
+    // }
+
+  }
   
 }
